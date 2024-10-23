78554bbb2db614e5cad89cf349ec0f15ddbe3d36
Pablo <pablocortes1101@gmail.com>
2024-10-23 02:21:07 -0300

    fix: add password to client schema

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,46 +1,23 @@
-import { PrismaClient } from "@prisma/client"
-import bcrypt from "bcrypt";
-import fs from "fs";
-import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
+import { users, clients } from "./data.js";
 
-dotenv.config();
-
-const prisma = new PrismaClient()
-
-async function hashPassword(password) {
-  return await bcrypt.hash(password, 10);
-}
-
-async function createOrUpdatePerson(person, hashedPassword) {
-  return await prisma.person.upsert({
-    where: { rut: person.rut },
-    update: {},
-    create: {
-      name: person.name,
-      email: person.email,
-      rut: person.rut,
-      password: hashedPassword,
-      companyRut: person.companyRut,
-      isClient: person.isClient,
-      tickets: {
-        create: person.tickets ? person.tickets.map((ticket) => ({
-          title: ticket.title,
-          description: ticket.description,
-          status: ticket.status,
-        })) : [],
-      },
-    },
-  });
-}
+const prisma = new PrismaClient();
 
 async function main() {
-  const data = JSON.parse(fs.readFileSync("datatest.json", "utf-8"));
-  
-  await Promise.all(data.map(async (person) => {
-    const hashedPassword = await hashPassword(person.password);
-    const createdPerson = await createOrUpdatePerson(person, hashedPassword);
-    console.log(`Person created or updated: ${createdPerson.name}`);
-  }));
+  for (const user of users) {
+    await prisma.user.upsert({
+      where: { email: user.email },
+      update: {},
+      create: user,
+    });
+  }
+  for (const client of clients) {
+    await prisma.client.upsert({
+      where: { email: client.email },
+      update: {},
+      create: client,
+    });
+  }
 }
 
 main()
@@ -48,7 +25,7 @@ main()
     await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error('Unexpected error:', e);
+    console.error("Unexpected error:", e);
     await prisma.$disconnect();
     process.exit(1);
   });
