c8346e5cb422d6eed1cb21d2ebf4626829cbcb82
Pablo <pablocortes1101@gmail.com>
2024-10-27 21:43:52 -0300

    feat: add middleware to check user role

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,36 @@
 import withAuth, { NextRequestWithAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
 export default withAuth(
   function middleware(req: NextRequestWithAuth) {
-    console.log(req.nextUrl.pathname);
-    console.log(req.nextauth.token);
+    if (
+      req.nextUrl.pathname.startsWith("/admin") &&
+      req.nextauth.token?.role !== "admin"
+    ) {
+      return NextResponse.rewrite(new URL("/unauthorized", req.url));
+    }
+
+    if (
+      req.nextUrl.pathname.startsWith("/user") &&
+      req.nextauth.token?.role !== "user"
+    ) {
+      return NextResponse.rewrite(new URL("/unauthorized", req.url));
+    }
+
+    if (
+      req.nextUrl.pathname.startsWith("/client") &&
+      req.nextauth.token?.role !== "client"
+    ) {
+      return NextResponse.rewrite(new URL("/unauthorized", req.url));
+    }
   },
   {
     callbacks: {
-      authorized: ({ token }) => token?.role === "Admin",
+      authorized: ({ token }) => !!token,
     },
   }
 );
 
 export const config = {
-  matcher: ["/", "/tickets/create"],
+  matcher: ["/", "/admin(.*)", "/user(.*)", "/client(.*)"],
 };
