a247e0f54b22dbe8a55eb9c28f699657ebdb68cf
nachitopopwine <113112079+nachitopopwine@users.noreply.github.com>
2024-11-04 12:21:47 -0300

    update get from api/tickets/route.ts

--- app/api/tickets/route.ts
+++ app/api/tickets/route.ts
@@ -112,7 +112,6 @@ export async function GET(request: Request) {
       },
     });
 
-    // Traducir y formatear tickets
     const translatedTickets = tickets.map((ticket) => {
       const translatedPriority = {
         [Priority.Low]: "Baja",
