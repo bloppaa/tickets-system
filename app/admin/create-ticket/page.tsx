a9a1b3e5f6d492d60d62e82bc0fd0a66e5cd3b28
Pablo <pablocortes1101@gmail.com>
2024-11-12 08:28:36 -0300

    feat: add POST method to create ticket given client rut

--- app/admin/create-ticket/page.tsx
+++ app/admin/create-ticket/page.tsx
@@ -23,10 +23,11 @@ import {
 import { Textarea } from "@/components/ui/textarea";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+import { translations } from "@/prisma/translations";
 
 const CreateTicketPage = () => {
   const router = useRouter();
-  const { handleSubmit } = useForm();
+  const { handleSubmit, register, setValue } = useForm();
   const [clientError, setClientError] = React.useState<string | null>(null);
 
   const onSubmit = handleSubmit(async (data) => {
@@ -47,7 +48,7 @@ const CreateTicketPage = () => {
       return;
     }
 
-    const response = await fetch("/api/tickets", {
+    const response = await fetch(`/api/tickets?clientRut=${ticketData.rut}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -80,8 +81,8 @@ const CreateTicketPage = () => {
                 <Label htmlFor="title">RUT del cliente</Label>
                 <Input
                   id="clientRut"
-                  name="clientRut"
                   placeholder="12345678-9"
+                  {...register("clientRut")}
                 />
                 {clientError && (
                   <span className="text-red-500 text-xs">{clientError}</span>
@@ -91,33 +92,37 @@ const CreateTicketPage = () => {
                 <Label htmlFor="title">Título</Label>
                 <Input
                   id="title"
-                  name="title"
                   placeholder="Resumen breve del problema"
+                  {...register("title")}
                 />
               </div>
               <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="type">Tipo de problema</Label>
-                <Select name="type">
+                <Select onValueChange={(value) => setValue("type", value)}>
                   <SelectTrigger id="type">
                     <SelectValue placeholder="Selecciona el tipo de problema" />
                   </SelectTrigger>
                   <SelectContent position="popper">
-                    <SelectItem value="hardware">Hardware</SelectItem>
-                    <SelectItem value="software">Software</SelectItem>
-                    <SelectItem value="other">Otro</SelectItem>
+                    {Object.entries(translations.type).map(([key, value]) => (
+                      <SelectItem key={key} value={key}>
+                        {value}
+                      </SelectItem>
+                    ))}
                   </SelectContent>
                 </Select>
               </div>
               <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="priority">Prioridad</Label>
-                <Select name="priority">
+                <Select onValueChange={(value) => setValue("priority", value)}>
                   <SelectTrigger id="priority">
                     <SelectValue placeholder="Selecciona la prioridad" />
                   </SelectTrigger>
                   <SelectContent position="popper">
-                    <SelectItem value="low">Baja</SelectItem>
-                    <SelectItem value="medium">Media</SelectItem>
-                    <SelectItem value="high">Alta</SelectItem>
+                    {Object.entries(translations.priority).map(
+                      ([key, value]) => (
+                        <SelectItem key={key} value={key}>
+                          {value}
+                        </SelectItem>
+                      )
+                    )}
                   </SelectContent>
                 </Select>
               </div>
@@ -125,8 +130,7 @@ const CreateTicketPage = () => {
                 <Label htmlFor="description">Descripción del problema</Label>
                 <Textarea
                   id="description"
-                  name="description"
-                  placeholder="Describe el problema en detalle"
+                  {...register("description")}
                   required
                 />
               </div>
